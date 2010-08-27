11855c080e9dbe64db4ef3e0620df85687980824
Petr Chalupa (Pitr) <gaul@post.cz>
2010-08-27 14:55:00 +0200

    Examples
    see http://www.pivotaltracker.com/story/show/4464900

---
+++ app/ask/counter.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+module Examples
+  module Ask
+    class Counter < Examples::Counter
+
+      define_widget do
+
+        # adds links to answer the number (counter) or
+        # to answer nothing.
+        # Everything else is as we need.
+        def actions
+          link_to('Add number').action { answer!(counter) }
+          link_to('Cancel').action { answer! }
+        end
+      end
+    end
+
+  end
+end
